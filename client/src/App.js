c0b3b60945821eebdaaba69934b3040cbbbf1c1f
George Dellas <geordellas@gmail.com>
2022-07-31 00:35:46 +0200

    Installed Cors & Axios - Connected to MySQL - Saved first user to the database

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,41 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Axios from "axios";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const addUser = () => {
+    Axios.post("http://localhost:3001/create", {
+      username: username,
+      password: password,
+    }).then(() => {
+      console.log("Success");
+    });
+  };
+
   return (
     <div className="App">
+      <div>
+        <label>Username: </label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <br />
+        <label>Password: </label>
+        <input
+          type="password"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+        <br />
+        <button onClick={addUser}> Login </button>
+      </div>
     </div>
   );
 }
