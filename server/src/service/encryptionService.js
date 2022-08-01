603786bba6fcccce138a2cbbf0769b14bc2b162a
George Dellas <geordellas@gmail.com>
2022-08-01 22:34:49 +0200

    Installed bcrypt library - Added encryptionService

---
+++ server/src/service/encryptionService.js
@@ -0,0 +1,8 @@
+import bcrypt from 'bcrypt';
+async function hashPassword(password) {
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+  return bcrypt.hash(password, salt);
+}
+
+export default hashPassword;
