293268a4659a91616cb6f31be9e8baead019bd6f
George Dellas <geordellas@gmail.com>
2022-08-01 16:49:27 +0200

    Moved db into src

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const { User } = require("./db/models");
+const { User } = require("./src/db/models");
 const cors = require("cors");
 
 app.use(cors());
