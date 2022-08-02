11c3c99a99ebc4f29f33213cd34f66418ffa76e6
George Dellas <geordellas@gmail.com>
2022-08-02 21:52:33 +0200

    Added sequelize.sync

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { api } from "./src/routes/index.js";
 import cors from "cors";
+import db from "./src/db/models/index.js";
 
 const app = express();
 
 app.use(cors());
 app.use("/api", api);
 
-app.listen(3001, () => {
-  console.log("Server is up and running");
+db.sequelize.sync().then((req) => {
+  app.listen(3001, () => {
+    console.log("Server is up and running");
+  });
 });
+
